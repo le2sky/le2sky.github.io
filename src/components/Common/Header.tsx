dc6174dfea63f2ad750454c60589373406da79cf
leesky <leehaneul0623@gmail.com>
2021-11-28 22:58:11 +0900

    new post! and change theme

--- src/components/Common/Header.tsx
+++ src/components/Common/Header.tsx
@@ -1,163 +1,174 @@
-import { FunctionComponent, useState } from "react";
-import styled from "@emotion/styled";
+import { FunctionComponent, useState } from 'react'
+import styled from '@emotion/styled'
 
 //icons
-import MenuIcon from '@mui/icons-material/Menu';
-import GitIcon from '@mui/icons-material/GitHub';
-import InstaIcon from '@mui/icons-material/Instagram';
-import MailIcon from '@mui/icons-material/Mail';
-import InfoIcon from '@mui/icons-material/Info';
-import PostIcon from '@mui/icons-material/Book';
-import CloseIcon from '@mui/icons-material/Close';
-import PortIcon from '@mui/icons-material/Person';
-
-
+import MenuIcon from '@mui/icons-material/Menu'
+import GitIcon from '@mui/icons-material/GitHub'
+import InstaIcon from '@mui/icons-material/Instagram'
+import MailIcon from '@mui/icons-material/Mail'
+import InfoIcon from '@mui/icons-material/Info'
+import PostIcon from '@mui/icons-material/Book'
+import CloseIcon from '@mui/icons-material/Close'
+import PortIcon from '@mui/icons-material/Person'
 
 //meterial
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import { Button, Card, CardContent, IconButton, Typography } from "@mui/material";
-import Snackbar from '@mui/material/Snackbar';
-import Slide from '@mui/material/Slide';
-import { Link } from "gatsby";
-
+import Box from '@mui/material/Box'
+import Drawer from '@mui/material/Drawer'
+import List from '@mui/material/List'
+import Divider from '@mui/material/Divider'
+import ListItem from '@mui/material/ListItem'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import {
+  Button,
+  Card,
+  CardContent,
+  IconButton,
+  Typography,
+} from '@mui/material'
+import Snackbar from '@mui/material/Snackbar'
+import Slide from '@mui/material/Slide'
+import { Link } from 'gatsby'
 
 function TransitionUp(props: any) {
-    return <Slide {...props} direction="up" />
+  return <Slide {...props} direction="up" />
 }
-
-
 
 const HeaderWrapper = styled.header`
-    display: flex;
-    flex-direction: row;
-
+  display: flex;
+  flex-direction: row;
 `
 const LinkWrapper = styled.div`
-    margin-left: auto;
+  margin-left: auto;
+  margin-right: 10px;
 `
 const Header: FunctionComponent = () => {
-    const [state, setState] = useState(false);
+  const [state, setState] = useState(false)
 
-    //snack bar open state
-    const [open, setOpen] = useState(false);
+  //snack bar open state
+  const [open, setOpen] = useState(false)
 
-    //snack bar handler
-    const handleClick = () => {
-        setOpen(true);
-    };
+  //snack bar handler
+  const handleClick = () => {
+    setOpen(true)
+  }
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+  const handleClose = () => {
+    setOpen(false)
+  }
 
-    //snack bar action
-    const action = (
-        <div>
-            <Button color="info" size="small">
-                COPY
-            </Button>
-            <IconButton
-                size="small"
-                aria-label="close"
-                color="inherit"
-                onClick={handleClose}
-            >
-                <CloseIcon fontSize="small" />
-            </IconButton>
-        </div>
-    );
+  //snack bar action
+  const action = (
+    <div>
+      <Button color="info" size="small">
+        COPY
+      </Button>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </div>
+  )
 
+  //side menu
+  const toggleButton = () => {
+    setState(!state)
+  }
 
-
-    //side menu 
-    const toggleButton = () => {
-        setState(!state);
-    }
-
-    const list = () => (
-        <Box
-            sx={{ width: 250 }}
-            role="presentation"
-        >
-            <Card sx={{ minWidth: 275 }}>
-                <CardContent>
-                    <Typography variant="h6" component="div">
-                        le2sky's tech blog🚀
-                    </Typography>
-                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                        1999.06.23 🐰
-                    </Typography>
-                    <br />
-                    <Typography sx={{ fontSize: 12 }} variant="body2">
-                        "개발과 운동을 좋아하는 이하늘입니다."<br />
-                        백엔드 개발자가 되기 위해서 <br />
-                        학습 중입니다.💻
-                    </Typography>
-                </CardContent>
-            </Card>
-            <List>
-                {['블로그 소개', '게시글'].map((text, index) => (
-                    <ListItem button key={text} component={Link} to={index === 0 ? "/info" : ""}>
-                        <ListItemIcon>
-                            {index === 0 ? <InfoIcon /> :
-                                index === 1 ? <PostIcon /> : null
-                            }
-                        </ListItemIcon>
-                        <ListItemText primary={text} />
-                    </ListItem>
-                ))}
-            </List>
-            <Divider />
-            <List>
-                {['Github', 'Instagram', 'Portfolio'].map((text, index) => (
-                    <ListItem button key={text} component={Link}
-                        to={text === 'Github' ? "https://github.com/le2sky" :
-                            text === "Instagram" ? "https://www.instagram.com/lee.___.sky/" :
-                                text === "Portfolio" ? "/404" : "/404"
-                        }>
-                        <ListItemIcon>
-                            {index === 0 ? <GitIcon /> :
-                                index === 1 ? <InstaIcon /> :
-                                    index === 2 ? <PortIcon /> : null
-                            }
-                        </ListItemIcon>
-                        <ListItemText primary={text} />
-                    </ListItem>
-                ))}
-            </List>
-        </Box>
-    );
-    return (
-        <HeaderWrapper>
-            <Button onClick={toggleButton} color="secondary" variant="text">{<MenuIcon />}</Button>
-            <Drawer
-                open={state}
-                onClose={toggleButton}
-            >
-                {list()}
-            </Drawer>
-            <LinkWrapper>
-                <Button href="https://github.com/le2sky" color="secondary" variant="text">{<GitIcon />}</Button>
-                <Button href="https://www.instagram.com/lee.___.sky/" color="secondary" variant="text">{<InstaIcon />}</Button>
-                <Button onClick={handleClick} color="secondary" variant="text">{<MailIcon />}</Button>
-            </LinkWrapper>
-            <Snackbar
-                open={open}
-                onClose={handleClose}
-                TransitionComponent={TransitionUp}
-                message="le2sky@kakao.com"
-                key={TransitionUp ? TransitionUp.name : ''}
-                anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-                action={action}
-            />
-        </HeaderWrapper >
-    );
+  const list = () => (
+    <Box sx={{ width: 250 }} role="presentation">
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography variant="h6" component="div">
+            le2sky's tech blog🚀
+          </Typography>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            1999.06.23 🐰
+          </Typography>
+          <br />
+          <Typography sx={{ fontSize: 12 }} variant="body2">
+            "개발과 운동을 좋아하는 이하늘입니다."
+            <br />
+            백엔드 개발자가 되기 위해서 <br />
+            학습 중입니다.💻
+          </Typography>
+        </CardContent>
+      </Card>
+      <List>
+        {['블로그 소개', '게시글'].map((text, index) => (
+          <ListItem
+            button
+            key={text}
+            component={Link}
+            to={index === 0 ? '/info' : ''}
+          >
+            <ListItemIcon>
+              {index === 0 ? <InfoIcon /> : index === 1 ? <PostIcon /> : null}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['Github', 'Instagram', 'Portfolio'].map((text, index) => (
+          <ListItem
+            button
+            key={text}
+            component={Link}
+            to={
+              text === 'Github'
+                ? 'https://github.com/le2sky'
+                : text === 'Instagram'
+                ? 'https://www.instagram.com/lee.___.sky/'
+                : text === 'Portfolio'
+                ? '/404'
+                : '/404'
+            }
+          >
+            <ListItemIcon>
+              {index === 0 ? (
+                <GitIcon />
+              ) : index === 1 ? (
+                <InstaIcon />
+              ) : index === 2 ? (
+                <PortIcon />
+              ) : null}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  )
+  return (
+    <HeaderWrapper>
+      <IconButton onClick={toggleButton}>{<MenuIcon />}</IconButton>
+      <Drawer open={state} onClose={toggleButton}>
+        {list()}
+      </Drawer>
+      <LinkWrapper>
+        <IconButton href="https://github.com/le2sky">{<GitIcon />}</IconButton>
+        <IconButton href="https://www.instagram.com/lee.___.sky/">
+          {<InstaIcon />}
+        </IconButton>
+        <IconButton onClick={handleClick}>{<MailIcon />}</IconButton>
+      </LinkWrapper>
+      <Snackbar
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={TransitionUp}
+        message="le2sky@kakao.com"
+        key={TransitionUp ? TransitionUp.name : ''}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        action={action}
+      />
+    </HeaderWrapper>
+  )
 }
 
-export default Header;
+export default Header
--- src/components/Main/Introduction.tsx
+++ src/components/Main/Introduction.tsx
@@ -1,69 +1,66 @@
-import React, { FunctionComponent } from "react";
-import styled from "@emotion/styled";
-import ProfileImage from "./ProfileImage";
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+import ProfileImage from './ProfileImage'
 
-import { IGatsbyImageData } from "gatsby-plugin-image";
-
+import { IGatsbyImageData } from 'gatsby-plugin-image'
 
 const Background = styled.div`
-    width: 100%;
-    color: #FFFFFF;
-    background-color: #4158D0;
-    background-image: linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%);
-    
+  width: 100%;
+  color: #ffffff;
+  background-color: #4158d0;
+  background-image: linear-gradient(90deg, #485563 0%, #29323c 100%);
 `
 
 const Wrapper = styled.div`
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: flex-start;
-        width: 768px;
-        height: 400px;
-        margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  width: 768px;
+  height: 400px;
+  margin: 0 auto;
 
-        @media (max-width: 768px) {
-            width: 100%;
-            height: 300px;
-            padding: 0 20px;
-        }    
+  @media (max-width: 768px) {
+    width: 100%;
+    height: 300px;
+    padding: 0 20px;
+  }
 `
 const SubTitle = styled.div`
-        font-size: 18px;
-        font-weight: 400;
+  font-size: 18px;
+  font-weight: 400;
 
-        @media (max-width: 768px) {
-            font-size: 10px
-        }   
+  @media (max-width: 768px) {
+    font-size: 10px;
+  }
 `
 
 const Title = styled.div`
-        margin-top: 5px;
-        font-size: 35px;
-        font-weight: 700;
-        @media (max-width: 768px) {
-            font-size: 25px
-        } 
+  margin-top: 5px;
+  font-size: 35px;
+  font-weight: 700;
+  @media (max-width: 768px) {
+    font-size: 25px;
+  }
 `
 
-
 type IntroductionProps = {
-    profileImage: IGatsbyImageData
+  profileImage: IGatsbyImageData
 }
 
 const Introduction: FunctionComponent<IntroductionProps> = function ({
-    profileImage,
+  profileImage,
 }) {
-    return (
-        <Background>
-            <Wrapper>
-                <ProfileImage profileImage={profileImage} />
-                <SubTitle>ㅡ</SubTitle>
-                <SubTitle>창의적인 아이디어를 위해 끊임없이 탐구합니다.</SubTitle>
-                <Title>Ha-Neul Lee</Title>
-            </Wrapper>
-        </Background >
-    )
+  return (
+    <Background>
+      <Wrapper>
+        <ProfileImage profileImage={profileImage} />
+        <SubTitle>ㅡ</SubTitle>
+        <SubTitle>창의적인 아이디어를 위해 끊임없이 탐구합니다.</SubTitle>
+        <Title>Ha-Neul Lee</Title>
+      </Wrapper>
+    </Background>
+  )
 }
 
 export default Introduction
