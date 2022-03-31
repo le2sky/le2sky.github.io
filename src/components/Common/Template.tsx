caab3a81ed7f897a3aeb02f3b42c24a725e8daaa
leesky <leehaneul0623@gmail.com>
2022-03-31 22:32:12 +0900

    fix: add robot meta tag

--- src/components/Common/Template.tsx
+++ src/components/Common/Template.tsx
@@ -37,6 +37,8 @@ const Template: FunctionComponent<TemplateProps> = function ({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
 
+        <meta name="robots" content="index,follow"/>
+
         <meta property="og:type" content="website" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
